69af79aa18ec826cab95bb616efab06d4a08a093
Ben Shiffman <notbenshiffman@gmail.com>
2017-11-06 13:51:47 -0800

    GLSurfaceView basic implementation

--- app/src/main/java/com/example/bshiffman5629/reeperg/MyGLRenderer.java
+++ app/src/main/java/com/example/bshiffman5629/reeperg/MyGLRenderer.java
@@ -3,6 +3,8 @@ package com.example.bshiffman5629.reeperg;
 import android.graphics.Path;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.os.SystemClock;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -13,6 +15,13 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
 
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    /*private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+
+    private float[] mRotationMatrix = new float[16];*/
+
     private Triangle        mTriangle;
     private Square          mSquare;
     private Quadrilateral   mQuadrilateral;
@@ -20,14 +29,14 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     private Paths           mPaths;
 
     static float shapeCoords[] = { //counter-clockwise
-            0.0f,  0.75f, 0.0f,   // 0
-            -0.5f, 0.5f, 0.0f,   // 1
-            -0.5f, 0.0f, 0.0f,  // 2
-            0.5f,  0.0f, 0.0f, // 3
-            0.5f, 0.5f, 0.0f }; // 4
+            /*0.0f,  0.65f, 0.0f,*/   // 0
+            -0.25f, 0.5f, 0.0f,   // 1
+            -0.25f, -0.5f, 0.0f,  // 2
+            0.25f,  -0.5f, 0.0f, // 3
+            0.25f, 0.5f, 0.0f }; // 4
     private short shapeDrawOrder[] = {0, 1, 2,
-                                      0, 2, 3,
-                                      0, 3, 4};
+                                      0, 2, 3/*,
+                                      0, 3, 4*/};
 
     static float pathsCoords[] = { //counter-clockwise
             0.0f,  0.75f, 0.0f,   // 0
@@ -56,18 +65,47 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     }
 
     public void onDrawFrame(GL10 unused) {
+        //float[] scratch = new float[16];
+
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        // Set the camera position (View matrix)
+        /*Matrix.setLookAtM(mViewMatrix, 0, //Matrix?, wut
+                            0,      0,      -3,     //X tilt        Y tilt      Z tilt (zoom?) only -3
+                            0f,     0f,     0f,     //smush X       smush Y     smush Z
+                            0.0f,   1.0f,   0.0f);  //X vector      Y vector    Z vector*/
+
+        // Calculate the projection and view transformation
+        //Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        // Create a rotation transformation for the triangle
+        //long time = SystemClock.uptimeMillis() % 4000L;
+        //
+        // float angle = 0.090f * ((int) time);
+        //Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, 1.0f);
+
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the mMVPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        //Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
 
         //mTriangle.draw();
         //mSquare.draw();
         //mQuadrilateral.draw();
-        //mShape.draw();
-        mPaths.draw();
+        mShape.draw(/*mMVPMatrix, scratch*/);
+        //mPaths.draw();
     }
 
+    @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+
+        //float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        //Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
     public static int loadShader(int type, String shaderCode){
@@ -81,6 +119,16 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         GLES20.glCompileShader(shader);
 
         return shader;
+    }
+
+    public volatile float mAngle;
+
+    public float getAngle() {
+        return mAngle;
+    }
+
+    public void setAngle(float angle) {
+        mAngle = angle;
     }
 
 }
--- app/src/main/java/com/example/bshiffman5629/reeperg/MyGLSurfaceView.java
+++ app/src/main/java/com/example/bshiffman5629/reeperg/MyGLSurfaceView.java
@@ -2,7 +2,9 @@ package com.example.bshiffman5629.reeperg;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.text.method.Touch;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 
 /**
@@ -12,6 +14,10 @@ import android.view.MotionEvent;
 public class MyGLSurfaceView extends GLSurfaceView {
 
     private final MyGLRenderer mRenderer;
+
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    private float mPreviousX;
+    private float mPreviousY;
 
     public MyGLSurfaceView(Context context){
         super(context);
@@ -25,10 +31,79 @@ public class MyGLSurfaceView extends GLSurfaceView {
         setRenderer(mRenderer);
 
         // Render the view only when there is a change in the drawing data
-        //setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
     }
 
-    /*public boolean onTouchEvent (MotionEvent e) {
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
 
-    }*/
+        float x = e.getX();
+        float y = e.getY();
+
+        //coordinates for e start in top left corner of screen
+        //up
+        if(y<getHeight()/4 && getWidth()/4<x && x<getWidth()*3/4 && MotionEvent.ACTION_DOWN == e.getAction()){
+            for(int i=1; i<=(MyGLRenderer.shapeCoords.length+1)/3-1; i+=3){
+                MyGLRenderer.shapeCoords[i]+=0.05f;
+            }
+            Log.d("Up x",""+MyGLRenderer.shapeCoords[0]);
+            Log.d("Up y",""+MyGLRenderer.shapeCoords[1]);
+            requestRender();
+        }
+        //down
+        else if(y>getHeight()*3/4 && getWidth()/4<x && x<getWidth()*3/4 && MotionEvent.ACTION_DOWN == e.getAction()){
+            for(int i=1; i<=(MyGLRenderer.shapeCoords.length+1)/3-1; i+=3){
+                MyGLRenderer.shapeCoords[i]-=0.05f;
+            }
+            requestRender();
+            Log.d("Down x",""+MyGLRenderer.shapeCoords[0]);
+            Log.d("Down y",""+MyGLRenderer.shapeCoords[1]);
+        }
+        //left
+        else if(x<getWidth()/4 && getHeight()/4<y && y<getHeight()*3/4 && MotionEvent.ACTION_DOWN == e.getAction()){
+            for(int i=0; i<=(MyGLRenderer.shapeCoords.length+1)/3-1; i+=3){
+                MyGLRenderer.shapeCoords[i]-=0.05f;
+            }
+            requestRender();
+            Log.d("Left x",""+MyGLRenderer.shapeCoords[0]);
+            Log.d("Left y",""+MyGLRenderer.shapeCoords[1]);
+        }
+        //right
+        else if(x>getWidth()*3/4 && getHeight()/4<y && y<getHeight()*3/4 && MotionEvent.ACTION_DOWN == e.getAction()){
+            for(int i=0; i<=(MyGLRenderer.shapeCoords.length+1)/3-1; i+=3){
+                MyGLRenderer.shapeCoords[i]+=0.05f;
+            }
+            requestRender();
+            Log.d("Right x",""+MyGLRenderer.shapeCoords[0]);
+            Log.d("Right y",""+MyGLRenderer.shapeCoords[1]);
+        }
+        /*switch (e.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+
+                float dx = x - mPreviousX;
+                float dy = y - mPreviousY;
+
+                // reverse direction of rotation above the mid-line
+                if (y > getHeight() / 2) {
+                    dx = dx * -1 ;
+                }
+
+                // reverse direction of rotation to left of the mid-line
+                if (x < getWidth() / 2) {
+                    dy = dy * -1 ;
+                }
+
+                mRenderer.setAngle(
+                        mRenderer.getAngle() +
+                                ((dx + dy) * TOUCH_SCALE_FACTOR));
+                requestRender();
+        }*/
+
+        mPreviousX = x;
+        mPreviousY = y;
+        return true;
+    }
 }
