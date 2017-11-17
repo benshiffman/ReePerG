79051d6c21fa4aca7c7d4998de512f100b073abd
Ben Shiffman <notbenshiffman@gmail.com>
2017-11-17 14:06:45 -0800

    GLSurfaceView basic implementation

--- app/src/main/java/com/example/bshiffman5629/reeperg/MyGLRenderer.java
+++ app/src/main/java/com/example/bshiffman5629/reeperg/MyGLRenderer.java
@@ -83,14 +83,13 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     }
 
     public void onDrawFrame(GL10 unused) {
-
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
         //mTriangle.draw();
         //mSquare.draw();
         //mQuadrilateral.draw();
-        mShape.draw(/*mMVPMatrix, scratch*/);
+        mShape.draw();
         mPaths.draw();
         Log.d("Test",""+"Success");
     }
@@ -98,6 +97,8 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+
+        mShape = new Shape(shapeCoords, shapeDrawOrder);
     }
 
     public static int loadShader(int type, String shaderCode){
--- app/src/main/java/com/example/bshiffman5629/reeperg/MyGLSurfaceView.java
+++ app/src/main/java/com/example/bshiffman5629/reeperg/MyGLSurfaceView.java
@@ -32,6 +32,6 @@ public class MyGLSurfaceView extends GLSurfaceView{
         setRenderer(mRenderer);
 
         // Render the view only when there is a change in the drawing data
-        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        //setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
     }
 }
