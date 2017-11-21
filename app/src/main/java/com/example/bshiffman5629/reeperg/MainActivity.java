3eff16af2b14998b362e14cc898f89a23f795b2c
nanderson0128 <nanderson.soccer@gmail.com>
2017-11-21 13:09:41 -0800

    stuff fixed

--- app/src/main/java/com/example/bshiffman5629/reeperg/MainActivity.java
+++ app/src/main/java/com/example/bshiffman5629/reeperg/MainActivity.java
@@ -88,95 +88,28 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
         }
         return true;
     }
-
-    /*@Override
-    public boolean onTouchEvent(MotionEvent e) {
-        // MotionEvent reports input details from the touch screen
-        // and other input controls. In this case, you are only
-        // interested in events where the touch position changed.
-
-        float x = e.getX();
-        float y = e.getY();
-
-        //coordinates for e start in top left corner of screen
-        //up
-        if(y<getHeight()/4 && getWidth()/4<x && x<getWidth()*3/4 && MotionEvent.ACTION_DOWN == e.getAction()){
-            for(int i=1; i<=MyGLRenderer.shapeCoords.length; i+=3){
-                MyGLRenderer.shapeCoords[i]+=0.05f;
-                //MyGLRenderer.mainInstance.mShape.shapeCoords[i]+=0.05f;
-            }
-            requestRender();
-            Log.d("Up x",""+MyGLRenderer.shapeCoords[0]);
-            Log.d("Up y",""+MyGLRenderer.shapeCoords[1]);
-        }
-        //down
-        else if(y>getHeight()*3/4 && getWidth()/4<x && x<getWidth()*3/4 && MotionEvent.ACTION_DOWN == e.getAction()){
-            for(int i=1; i<=MyGLRenderer.shapeCoords.length; i+=3){
-                MyGLRenderer.shapeCoords[i]-=0.05f;
-            }
-            requestRender();
-            Log.d("Down x",""+MyGLRenderer.shapeCoords[0]);
-            Log.d("Down y",""+MyGLRenderer.shapeCoords[1]);
-        }
-        //left
-        else if(x<getWidth()/4 && getHeight()/4<y && y<getHeight()*3/4 && MotionEvent.ACTION_DOWN == e.getAction()){
-            for(int i=0; i<=MyGLRenderer.shapeCoords.length-1; i+=3){
-                MyGLRenderer.shapeCoords[i]-=0.025f;
-            }
-            requestRender();
-            Log.d("Left x",""+MyGLRenderer.shapeCoords[0]);
-            Log.d("Left y",""+MyGLRenderer.shapeCoords[1]);
-        }
-        //right
-        else if(x>getWidth()*3/4 && getHeight()/4<y && y<getHeight()*3/4 && MotionEvent.ACTION_DOWN == e.getAction()){
-            for(int i=0; i<=MyGLRenderer.shapeCoords.length-1; i+=3){
-                MyGLRenderer.shapeCoords[i]+=0.025f;
-            }
-            requestRender();
-            Log.d("Right x",""+MyGLRenderer.shapeCoords[0]);
-            Log.d("Right y",""+MyGLRenderer.shapeCoords[1]);
-        }
-
-        return true;
-    }*/
-
     @Override
     public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
         return false;
     }
-
     @Override
     public boolean onDoubleTap(MotionEvent motionEvent) {
         return false;
     }
-
     @Override
     public boolean onDoubleTapEvent(MotionEvent motionEvent) {
         return false;
     }
-
     @Override
-    public void onShowPress(MotionEvent motionEvent) {
-
-    }
-
+    public void onShowPress(MotionEvent motionEvent) {}
     @Override
     public boolean onSingleTapUp(MotionEvent motionEvent) {
         return false;
     }
-
     @Override
-    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        return false;
-    }
-
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {return false;}
     @Override
-    public void onLongPress(MotionEvent motionEvent) {
-
-    }
-
+    public void onLongPress(MotionEvent motionEvent) {}
     @Override
-    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        return false;
-    }
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {return false;}
 }
