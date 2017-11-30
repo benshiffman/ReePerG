b6c158456d1565f804a9cbe506bed448d0cf36a0
c1fr1 <c1fr1.stkcps@gmail.com>
2017-11-30 17:59:31 +0000

    improved spawn

--- app/src/main/java/com/example/bshiffman5629/reeperg/MyGLRenderer.java
+++ app/src/main/java/com/example/bshiffman5629/reeperg/MyGLRenderer.java
@@ -135,7 +135,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         gesturePath = new Paths(new float[]{0f, 0f, 0f}, new short[]{0, 0});
         gesturePath.color = new float[]{1f, 0f, 0f, 1f};
 
-        mainPlayer = new Player(4000, 1850, metrics);
+        mainPlayer = new Player(4200, 1850, metrics);
 
         float mapCoords[] = new float[map.length*3];
         short order[] = {0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17};
