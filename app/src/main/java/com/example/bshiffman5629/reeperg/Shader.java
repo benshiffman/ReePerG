6ce2894e1af65538fdfc78e9283a85f101413014
c1fr1 <c1fr1.stkcps@gmail.com>
2017-12-13 23:33:03 +0000

    changed how the shader files were loaded, shader still not working.

--- app/src/main/java/com/example/bshiffman5629/reeperg/Shader.java
+++ app/src/main/java/com/example/bshiffman5629/reeperg/Shader.java
@@ -1,8 +1,10 @@
 package com.example.bshiffman5629.reeperg;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.opengl.GLES20;
 import android.opengl.GLES30;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,6 +12,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Varas on 04/12/2017.
@@ -17,37 +20,30 @@ import java.util.List;
 
 public abstract class Shader {
     public int programID;
-    public Shader(Context ctx, String vertPath, String fragPath) {
+    public Shader(Context ctx, int vertID, int fragID) {
         String shaderCode = "";
-        try {
-            InputStream is = ctx.getAssets().open(vertPath);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                shaderCode += line;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        Resources resources = MainActivity.mainInstance.getResources();
+        Scanner vScanner = new Scanner(resources.openRawResource(vertID));
+        Scanner fScanner = new Scanner(resources.openRawResource(fragID));
+        while (vScanner.hasNextLine()) {
+            shaderCode += vScanner.nextLine();
         }
         int vShader = GLES30.glCreateShader(GLES20.GL_VERTEX_SHADER);
         GLES30.glShaderSource(vShader, shaderCode);
         GLES30.glCompileShader(vShader);
-        try {
-            InputStream is = ctx.getAssets().open(fragPath);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                shaderCode += line;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        shaderCode = "";
+        while (fScanner.hasNextLine()) {
+            shaderCode += fScanner.nextLine();
         }
         int fShader = GLES30.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
         GLES30.glShaderSource(fShader, shaderCode);
         GLES30.glCompileShader(fShader);
+
         programID = GLES30.glCreateProgram();
         GLES30.glAttachShader(programID, vShader);
         GLES30.glAttachShader(programID, fShader);
+        GLES30.glLinkProgram(programID);
         //bindtexture
     }
     abstract void setUniforms();
